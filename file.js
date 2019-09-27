9629a5dd895b8ad4ba07e607bbd856b743178ec8
DrawH <pierre.rnm@gmail.com>
2019-09-27 15:54:05 +0200

    finish

--- file.js
+++ file.js
@@ -1,2 +1,21 @@
-console.log('bonsoir');
+const express = require('express');
+const request = require('request');
+const app = express();
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    res.render('search');
+});
+
+app.get('/results', function(req, res){
+    var query = req.query.search;
+    var url = 'https://www.omdbapi.com/?s=' + query + '&apikey=YOUR_API_KEY';
+    request(url, function(error, response, body){
+        if(!error && response.statusCode == 200){
+            var data = JSON.parse(body)
+            res.render('results', {data: data});
+        }
+    });
+});
+
 
--- server.js
+++ server.js
@@ -4,5 +4,5 @@ var server = http.createServer(function(req, res) {
   res.writeHead(200);
   res.end('Hello world');
 });
-server.listen(8080);
+server.listen(3000);
 
